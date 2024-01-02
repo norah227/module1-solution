20d7c5a48f104aabd802c70eb16a48dd09188552
norah <48625897+norah227@users.noreply.github.com>
2024-01-02 18:08:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-(function(){'use strict';angular.module('LunchCheck',[]).controller('LunchCheckController',function($scope){$scope.inputdata="";$scope.message="";$scope.checkdata=function(){var splitData=$scope.inputdata.split(",");console.log(splitData.length);if(splitData.length>3){document.getElementById("resultData").style.color="red";$scope.message="Too much!"}else if($scope.inputdata==""){document.getElementById("resultData").style.color="red";$scope.message="Please enter data first"}else{document.getElementById("resultData").style.color="green";$scope.message="Enjoy"}}})})()
+(function(){
+    'use strict';
+    
+        angular.module('LunchCheck', [])
+    
+        .controller('LunchCheckController', function($scope){
+    
+            $scope.inputdata = "";
+            $scope.message = "";
+            $scope.checkdata = function(){
+                var splitData = $scope.inputdata.split(",");
+                console.log(splitData.length);
+                if(splitData.length > 3)
+                {
+                    document.getElementById("resultData").style.color = "red";
+                    $scope.message = "Too much!";
+                }
+                else if($scope.inputdata == "")
+                {
+                    document.getElementById("resultData").style.color = "red";
+                    $scope.message = "Please enter data first";
+                }
+                else{
+                    document.getElementById("resultData").style.color = "green";
+                    $scope.message = "Enjoy";
+                }
+            }
+        });
+    })();
